b124fba637c610e56f9da35d79d846b20841fefc
Albert <nonono28693346@gmail.com>
2025-07-03 23:39:08 +0800

    Add Vue.js project files

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // 路由懶加載
+    component: () => import('../views/AboutView.vue')
+  },
+]
+
+const router = createRouter({
+  // 注意這裡的修改 - 使用import.meta.env.BASE_URL作為基礎路徑
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
