423a61afcd0be06ca44682c07307cc9376551076
Albert <nonono28693346@gmail.com>
2025-07-04 00:25:39 +0800

    hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
@@ -16,8 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  // 注意這裡的修改 - 使用import.meta.env.BASE_URL作為基礎路徑
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
